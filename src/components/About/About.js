a64b86c20d02d8cabc5e296165ab85926aaac926
Alvin <alvintsa@buffalo.edu>
2025-03-31 14:24:23 -0400

    added image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,54 +1,65 @@
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
+import profileImage from './profile.jpg' // Assuming the image is in the same folder
 import './About.css'
 
 const About = () => {
   const { name, role, description, resume, social } = about
 
   return (
-    <div className='about center'>
-      {name && (
-        <h1>
-          Hi! I'm <span className='about__name'>{name}</span>
-        </h1>
-      )}
+    <div className="about center">
+      <div className="about__content">
+        {/* Text Content */}
+        <div className="about__text">
+          {name && (
+            <h1>
+              Hi! I&apos;m <span className="about__name">{name}</span>
+            </h1>
+          )}
+          
+          {role && <h2 className="about__role">A {role}.</h2>}
+          <p className="about__desc">{description && description}</p>
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
-      <p className='about__desc'>{description && description}</p>
-
-      <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
+          <div className="about__contact center">
+            {resume && (
+              <a href={resume}>
+                <span type="button" className="btn btn--outline">
+                  Resume
+                </span>
               </a>
             )}
 
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
+            {social && (
+              <>
+                {social.github && (
+                  <a
+                    href={social.github}
+                    aria-label="github"
+                    className="link link--icon"
+                  >
+                    <GitHubIcon />
+                  </a>
+                )}
+
+                {social.linkedin && (
+                  <a
+                    href={social.linkedin}
+                    aria-label="linkedin"
+                    className="link link--icon"
+                  >
+                    <LinkedInIcon />
+                  </a>
+                )}
+              </>
             )}
-          </>
-        )}
+          </div>
+        </div>
+
+        {/* Profile Image */}
+        <div className="about__image">
+          <img src={profileImage} alt="Profile" />
+        </div>
       </div>
     </div>
   )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,22 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  title: "Alvin's Portfolio",
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Alvin',
-  role: 'Aspiring Software Engineer',
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  // role: 'Aspiring Software Engineer',
+  description: `I'm currently a Master's Student in Computer Science at the University at Buffalo, 
+  where I also received my Bachelor's in Computer Science with Minors in Japanese, Mathematics, and Sociology 
+  in 2023. Along with my current studies, I work with two professors, 
+  conducting research in NLP, AI, and Software; each in their respective labs: Computational Linguistics 
+  and Cognition (CaLiCo) Lab, and Computational and Equity (cUBe) Lab.`,
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/alvintsa/',
+    github: 'https://github.com/alvintsa',
   },
 }
 
@@ -49,23 +52,12 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  "Making Friends!", "Python", "JavaScript", "HTML", "CSS", "Scala", "C#", "Docker", "ReactJS", "NodeJS", "SQL", "MongoDB", "Digital Ocean"
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'alvinc2013@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
