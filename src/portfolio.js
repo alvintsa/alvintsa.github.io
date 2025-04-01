8ac9caeb1719e2af18a0048f4d0087da2d0810db
Alvin <alvintsa@buffalo.edu>
2025-03-31 20:03:37 -0400

    fixed link

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  homepage: 'https://alvintsa.github.io/',
   title: "Alvin's Portfolio",
 }
 
