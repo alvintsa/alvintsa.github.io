d81b40a80ecabfe3cd98a1964eb11762a856d85a
Alvin <alvintsa@buffalo.edu>
2025-04-05 19:49:56 -0400

    fixed hyperlinks

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,10 +12,10 @@ const about = {
   where I also received my Bachelor's in Computer Science with Minors in Japanese, Mathematics, and Sociology 
   in 2023. Along with my current studies, I work with two professors, 
   conducting research in NLP, AI, and Software; each in their respective labs: 
-  <a href="https://www.buffalo.edu/cs/research/calico.html" target="_blank" rel="noopener noreferrer" style="color: #ff6347; font-weight: bold;">
+  <a href="https://www.acsu.buffalo.edu/~cxjacobs/index/CaLiCo_Lab.html" target="_blank" rel="noopener noreferrer" style="color: #ff6347; font-weight: bold;">
     Computational Linguistics and Cognition (CaLiCo) Lab
   </a>, and
-  <a href="https://www.buffalo.edu/cs/research/cube.html" target="_blank" rel="noopener noreferrer" style="color: #1e90ff; font-weight: bold;">
+  <a href="https://cse.buffalo.edu/cubelab/" target="_blank" rel="noopener noreferrer" style="color: #1e90ff; font-weight: bold;">
     Computational and Equity (cUBe) Lab
   </a>.`,   
   resume: 'https://alvintsa.github.io/resume-page/alvin-tsang-resume.pdf',
